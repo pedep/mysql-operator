Bajrang/mysql-operator-1

Refresh mysql and orchestrator credentials

pkg/sidecar/app/watcher_suit_test.go
@@ -1,0 +1,33 @@
+/*
+Copyright 2019 Pressinfra SRL
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package app
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	logf "github.com/presslabs/controller-util/log"
+)
+
+func TestSidecarApp(t *testing.T) {
+	logf.SetLogger(logf.ZapLoggerTo(GinkgoWriter, true))
+
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Sidecar App Suite")
+}

pedep/mysql-operator

pkg/sidecar/watcher_suit_test.go
@@ -1,0 +1,33 @@
+/*
+Copyright 2019 Pressinfra SRL
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package sidecar
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	logf "github.com/presslabs/controller-util/log"
+)
+
+func TestSidecarApp(t *testing.T) {
+	logf.SetLogger(logf.ZapLoggerTo(GinkgoWriter, true))
+
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Sidecar App Suite")
+}